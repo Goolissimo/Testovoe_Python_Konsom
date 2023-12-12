3f3911eaacd280a7f9500224f258ea0074c42ca1
Савелий <113251478+Goolissimo@users.noreply.github.com>
2023-12-12 22:40:56 +0500

    Delete run.sh

--- run.sh
+++
@@ -1,8 +0,0 @@
-docker build -t docker_frontend -f frontend/Dockerfile .
-docker run -p 8501:8501 docker_frontend
-
-docker build -t docker_backend -f backend/Dockerfile .
-docker run -p 8000:8000 docker_backend
-
-docker-compose up -d --build
-docker-compose down
